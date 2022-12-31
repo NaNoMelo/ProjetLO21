8dcb32ddbe3a9bc86f210ac76769552ce6b095f2
NaNoMelo <nathan.lamey68@orange.fr>
2022-12-31 14:22:10 +0100

    CroiserPop (non fonctionnel)
    Signed-off-by: NaNoMelo <nathan.lamey68@orange.fr>

--- main.c
+++ main.c
@@ -1,44 +1,35 @@
 //
 // Created by natha on 08/12/2022.
 //
-#include
-<stdlib.h>
-#include
-<stdio.h>
-#include
-<time.h>
-#include
-"Individu/Individu.h"
-#include
-"Population/Population.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "Individu/Individu.h"
+#include "Population/Population.h"
 
-#define
-taillePopulation 100
-#define
-tailleIndividu 16
-#define
-nGen 200
-#define
-pCroise 0.5
+#define taillePopulation 100
+#define tailleIndividu 16
+#define nGen 200
+#define pCroise 0.5
 
 
 int main() {
 
-srand(time(NULL));
-Population population = createPopulation(taillePopulation, tailleIndividu);
-Population last;
+    srand(time(NULL));
+    Population population = createPopulation(taillePopulation, tailleIndividu);
+    Population last;
 
-for (int i = 0; i < nGen; i++) {
-population = croiserPopulation(population, pCroise);
-last = population;
-while (last->next != NULL) {
-last = last->next;
-}
-quick_sort(population, NULL);
-}
-afficherPopulation(population, "Population");
-quick_sort(population, last);
-afficherPopulation(population, "Population triée");
+    for (int i = 0; i < nGen; i++) {
+        population = croiserPopulation(population, pCroise);
+        last = population;
+        while (last->next != NULL) {
+            last = last->next;
+        }
+        quick_sort(population, NULL);
+    }
+    afficherPopulation(population, "Population");
+    quick_sort(population, last);
+    afficherPopulation(population, "Population triée");
 
-return 0;
+    return 0;
 }
